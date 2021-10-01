82490ef75f59352d66d293bec90c924789eefec6
Robert Zabrocki <robert.zabrocki.007@gmail.com>
2021-10-01 13:09:45 +0200

    Lekcja: "World Interactions and Enemies" zakończona

--- Assets/Art/Sprites/Scripts/RubyController.cs
+++ Assets/Art/Sprites/Scripts/RubyController.cs
@@ -6,9 +6,13 @@ public class RubyController : MonoBehaviour
 {
     public int maxHealth = 1;
     public float speed = 3.0f;
+    public float timeInvincible = 2.0f;
+
+    bool isInvincible;
 
     float horizontal;
     float vertical;
+    float invincibleTimer;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
@@ -27,6 +31,12 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer = invincibleTimer - Time.deltaTime;
+            if (invincibleTimer < 0) isInvincible = false;
+        }
     }
 
     void FixedUpdate()
@@ -40,6 +50,14 @@ public class RubyController : MonoBehaviour
 
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible) return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
