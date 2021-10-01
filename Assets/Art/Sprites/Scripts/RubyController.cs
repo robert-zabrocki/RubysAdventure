988b0a7591207c5201cec525d936e07b30564630
Robert Zabrocki <robert.zabrocki.007@gmail.com>
2021-10-01 12:07:29 +0200

    Lekcja: World Interactions - Collectibles zakończona

--- Assets/Art/Sprites/Scripts/RubyController.cs
+++ Assets/Art/Sprites/Scripts/RubyController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 1;
+    public float speed = 3.0f;
+
     float horizontal;
     float vertical;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -24,9 +32,16 @@ public class RubyController : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
     }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
 }
